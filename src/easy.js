cbf710c318ea034436e0e05b98e5311edb76ab44
cynthiateeters <glean@cynthiateeters.com>
2021-02-13 14:23:49 -0500

    Add easy

---
+++ src/easy.js
@@ -0,0 +1,11 @@
+/* easy.js */
+
+function removeObjProperty(arr, key) {
+  // return a new object which
+  // is a copy of the given object
+  // but with the property with the 
+  // given key removed
+  // do not change the given property
+
+}
+module.exports = removeObjProperty;
